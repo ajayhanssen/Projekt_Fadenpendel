485a56237940d800c164385452d847424ae1bb47
Andreas Thoeni <ta2244@mci4me.at>
2023-11-27 12:07:10 +0100

    naeherung hinzugefuegt

---
+++ pendel_kleine_a.m
@@ -0,0 +1,13 @@
+function uPunkt = pendel_kleine_a(~,u)
+% Modell Fadenpendel ohne Luftwiderstand
+g = 9.8; % Schwerbeschleunigung
+m = 1; % Masse der Kugel in kg
+l = 3; % Länge des Fadens in m
+r = 0.1; % Radius der Kugel in m
+J_kugel = 2/5 * m * r.^2; % Massenträgheitsmoment Kugel um Schwerp.
+J = m * l.^2; % Massenträgheitsmoment Kugel um Drehpunkt
+
+uPunkt = [u(2)
+          -m*g*l/J * u(1)];
+
+end
