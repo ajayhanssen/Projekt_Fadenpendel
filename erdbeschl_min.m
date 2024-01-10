b8a9fad4b79165a2cddc946f623bd23d180b7d61
ajayhanssen <ta2244@mci4me.at>
2024-01-10 10:36:26 +0100

    Beschleunigungsberechnung funktioniert(?)

--- erdbeschl_min.m
+++ erdbeschl_min.m
@@ -1,14 +1,10 @@
 function g_diff = erdbeschl_min(params, l, data)
     
     g = params(1);
-    phi0 = params(2);
+    phi0_punkt = params(2);
 
     t_lin = linspace(data(1,1), data(end,1), length(data(:,1)));
-    [~, u] = ode45(@(t,u) pendel_g_var(t,u,l,g), t_lin, [phi0 0]);
-    
-    %u
-    %data(:,2:3)
-    
+    [t, u] = ode45(@(t,u) pendel_g_var(t,u,l,g), t_lin, [0 phi0_punkt]);
 
     g_diff = sum(sum(u - data(:, 2:3).^2));
 
