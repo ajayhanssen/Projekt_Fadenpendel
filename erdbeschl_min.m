c3fea8ef37e16ea74760fb7a77ceb1f90cc4ed03
Andreas Thoeni <ta2244@mci4me.at>
2024-01-11 12:15:15 +0100

    beschleunigung funkt endlich

--- erdbeschl_min.m
+++ erdbeschl_min.m
@@ -3,9 +3,8 @@ function g_diff = erdbeschl_min(params, l, data)
     g = params(1);
     phi0_punkt = params(2);
 
-    t_lin = linspace(data(1,1), data(end,1), length(data(:,1)));
-    [t, u] = ode45(@(t,u) pendel_g_var(t,u,l,g), t_lin, [0 phi0_punkt]);
+    [t, u] = ode45(@(t,u) pendel_g_var(t,u,l,g), data(:,1), [0 phi0_punkt]);
 
-    g_diff = sum(sum(u - data(:, 2:3).^2));
+    g_diff = sum(sum((u - data(:, 2:3)).^2));
 
 end
