33a9f8d8f42830530bf60ac8390dda2c640f2acd
ajayhanssen <ta2244@mci4me.at>
2024-01-14 21:07:51 +0100

    kleine änderungen, export verbessert, blend-file aktualisiert, simulation gerendert wow

--- import_coordinates.py
+++ import_coordinates.py
@@ -12,9 +12,9 @@ blend_file_directory = os.path.dirname(blend_file_path)
 csv_relative_path = "coordinates.csv"
 csv_path = os.path.join(blend_file_directory, csv_relative_path)
 
-data = pd.read_csv(csv_path, sep=',', names=['l', 'r', 'x', 'y'])
+data = pd.read_csv(csv_path, sep=',', names=['l', 'x', 'y'])
 l = data['l'][0]
-r = data['r'][0]
+#r = data['r'][0]
 
 sphere = current_scene.objects['pendulum_sphere']
 hook_u = current_scene.objects['hook_upper']
@@ -22,7 +22,7 @@ focal_point = current_scene.objects['focal_point']
 
 def bake_keyframes(object):
     for index in range(data.shape[0]):
-        object.location = (data['x'][index], 0, data['y'][index] + l + r)
+        object.location = (data['x'][index], 0, data['y'][index] + l+l/10)
         object.keyframe_insert(data_path='location', frame=index)
     
 def reset_position(object):
@@ -35,10 +35,12 @@ def clear_keyframes(object):
 reset_position(sphere)
 clear_keyframes(sphere)
 
-sphere.scale = (r*2,r*2,r*2)
+#sphere.scale = (r*2,r*2,r*2)
 
-hook_u.location = (0.0, 0.0, l + r)
-focal_point.location = (0.0, 0.0, l/2 - r)
+hook_u.location = (0.0, 0.0, l+l/10)
+focal_point.location = (0.0, 0.0, l/2)
 
 bake_keyframes(sphere)
+
+current_scene.frame_end = len(data['x'])
 
